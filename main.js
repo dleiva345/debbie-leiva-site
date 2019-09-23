208fe115590aced0a33645f34ae80b41dc96fa68
Debbie Leiva <debbieleiva@yahoo.com>
2019-09-22 23:53:42 -0400

    pushing

--- main.js
+++ main.js
@@ -1,92 +1,36 @@
 $(document).ready(function() {
 
-    var animals = [
-      "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
-      "bird", "ferret", "turtle", "sugar glider", "chinchilla",
-      "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken",
-      "capybara", "teacup pig", "serval", "salamander", "frog"
-    ];
-  
-    // function to make buttons and add to page
-    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
-      $(areaToAddTo).empty();
-  
-      for (var i = 0; i < arrayToUse.length; i++) {
-        var a = $("<button>");
-        a.addClass(classToAdd);
-        a.attr("data-type", arrayToUse[i]);
-        a.text(arrayToUse[i]);
-        $(areaToAddTo).append(a);
-      }
-  
+  alert("hey are you there");
+
+   $("#myform").submit(function(){
+    var search = $("books").val();
+
+    if(search == '') {
+      alert("enter something in the field first");
     }
-  
-    $(document).on("click", ".animal-button", function() {
-      $("#animals").empty();
-      $(".animal-button").removeClass("active");
-      $(this).addClass("active");
-  
-      var type = $(this).attr("data-type");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
-  
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        .then(function(response) {
-          var results = response.data;
-  
-          for (var i = 0; i < results.length; i++) {
-            var animalDiv = $("<div class=\"animal-item\">");
-  
-            var rating = results[i].rating;
-  
-            var p = $("<p>").text("Rating: " + rating);
-  
-            var animated = results[i].images.fixed_height.url;
-            var still = results[i].images.fixed_height_still.url;
-  
-            var animalImage = $("<img>");
-            animalImage.attr("src", still);
-            animalImage.attr("data-still", still);
-            animalImage.attr("data-animate", animated);
-            animalImage.attr("data-state", "still");
-            animalImage.addClass("animal-image");
-  
-            animalDiv.append(p);
-            animalDiv.append(animalImage);
-  
-            $("#animals").append(animalDiv);
-          }
-        });
-    });
-  
-    $(document).on("click", ".animal-image", function() {
-  
-      var state = $(this).attr("data-state");
-  
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      }
-      else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
-  
-    $("#add-animal").on("click", function(event) {
-      event.preventDefault();
-      var newAnimal = $("input").eq(0).val();
-  
-      if (newAnimal.length > 2) {
-        animals.push(newAnimal);
-      }
-  
-      populateButtons(animals, "animal-button", "#animal-buttons");
-  
-    });
-  
-    populateButtons(animals, "animal-button", "#animal-buttons");
+    else{
+      var url = '';
+      var img = '';
+      var title = '';
+      var author = '';
+
+      $.get("https://www.googleapis.com/books/v1/volumes?q=" + search,function(response){
+        for(i=0;i<response.items.length;i++){
+          title=$('<h5 class="center-align white-text">' + response.items[i].volumeInfo.title + '</h5>');
+          title=$('<h5 class="center-align white-text">' + response.items[i].volumeInfo.authors + '</h5>');
+          img=$('<img class="aligning card z-depth-5" id="dynamic"><br><a href=' + response.items[i].volumeInfo.infolink + '><button id="imagebutton" class="btn red align-center">Read More</button></a>');
+          url=response.items[i].volumeInfo.imageLinks.thumbnail;
+
+          img.attr('src,url');
+          title.appendTo("#result");
+          author.appendTo("#result");
+          img.appendTo("#result");
+        }  
+      });
+    }
+
+
+   });
+    return false;
   });
   
